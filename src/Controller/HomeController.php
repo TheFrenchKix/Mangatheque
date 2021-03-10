4a95637ba6c2e5175fcc62a67d3b9439d2b37e10
SIO2 - Leo Pecqueux <c2e25>
2021-03-10 14:20:55 +0100

    Correction bug Index

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Manga;
+use App\Repository\MangaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,13 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(MangaRepository $mangaRepository): Response
     {
+        $mangas = $mangaRepository->findAll();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'mangas' => $mangas
         ]);
     }
 }
