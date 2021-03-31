7d2e82eecdaacaf89932f40d8c400568fa2e136b
SIO2 - Leo Pecqueux <c2e25>
2021-03-31 16:51:07 +0200

    Quelques modifs du 31/03 à 17h

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Role\RoleHierarchy;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
